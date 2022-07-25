38305aa7bac28f2b5561c9f2eda05272d6aa7005
孑愁 <401828628@qq.com>
2022-07-25 17:30:22 +0800

    修复ts文件描述

--- index.d.ts
+++ index.d.ts
@@ -3,23 +3,23 @@ export default class HwAnalytics {
      * 获取渠道名称 - 异步
      * Android only
      */
-    getChannel(): Promise<string>;
+    static getChannel(): Promise<string>;
 
     /**
      * 获取渠道名称 - 同步
      * Android only
      */
-    getChannelSync(): string;
+     static getChannelSync(): string;
 
     /**
      * 获取humeSDK版本号 - 异步
      * Android only
      */
-    getVersion(): Promise<string>;
+     static getVersion(): Promise<string>;
 
      /**
       * 获取humeSDK版本号 - 同步
       * Android only
       */
-    getVersionSync(): string;
+      static getVersionSync(): string;
 }
